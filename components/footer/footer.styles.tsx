83ad425d556fb8e730f59d00c6fca8a6558ad9cf
William Pei <william.h.pei@gmail.com>
2021-04-06 19:02:31 +1000

    refactor: move inline styles to new file

---
+++ components/footer/footer.styles.tsx
@@ -0,0 +1,27 @@
+import { ThemeUIStyleObject } from "@theme-ui/css";
+
+export const footerContainerCss: ThemeUIStyleObject = {
+  alignItems: "center",
+  flexFlow: "column",
+  m: "xl",
+};
+
+export const capsTextCss: ThemeUIStyleObject = {
+  variant: "text.caps",
+  mb: "md",
+};
+
+export const bodyTextLeftCss: ThemeUIStyleObject = {
+  variant: "text.body",
+  mr: "md",
+};
+
+export const bodyTextDividerCss: ThemeUIStyleObject = {
+  variant: "text.body",
+  color: "secondary",
+};
+
+export const bodyTextRightCss: ThemeUIStyleObject = {
+  variant: "text.body",
+  ml: "md",
+};
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,15 +1,16 @@
 /** @jsxImportSource theme-ui */
 import { Flex, Text } from "@theme-ui/components";
 import * as React from "react";
+import * as styles from "./footer.styles";
 
 const Footer: React.FC = () => {
   return (
-    <Flex as="footer" sx={{ alignItems: "center", flexFlow: "column", m: "xl" }}>
-      <Text sx={{ variant: "text.caps", mb: "md" }}>DESIGNED IN ADOBE XD & BUILT WITH NEXT JS</Text>
+    <Flex as="footer" sx={styles.footerContainerCss}>
+      <Text sx={styles.capsTextCss}>DESIGNED IN ADOBE XD & BUILT WITH NEXT JS</Text>
       <Flex>
-        <Text sx={{ variant: "text.body", mr: "md" }}>&copy; William Pei</Text>
-        <Text sx={{ variant: "text.body", color: "secondary" }}>\\</Text>
-        <Text sx={{ variant: "text.body", ml: "md" }}>last updated 21.04.06</Text>
+        <Text sx={styles.bodyTextLeftCss}>&copy; William Pei</Text>
+        <Text sx={styles.bodyTextDividerCss}>\\</Text>
+        <Text sx={styles.bodyTextRightCss}>last updated 21.04.06</Text>
       </Flex>
     </Flex>
   );
