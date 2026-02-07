ba521ad43cdb91a9294675fc257423bbe4f2c301
William Pei <draekien@users.noreply.github.com>
2026-02-07 19:09:40 +1100

    feat: theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
 	return (
 		<div>
 			<h1>Hello!</h1>
 			<ThemeToggle />
+			<Button variant="default">Primary</Button>
+			<Button variant="secondary">Secondary</Button>
+			<Button variant="outline">Outline</Button>
+			<Button variant="destructive">Destructive</Button>
+			<Button variant="ghost">Ghost</Button>
 		</div>
 	);
 }
