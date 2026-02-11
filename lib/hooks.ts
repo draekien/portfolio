caca2baf1708a9c8e4eac0beabde357f8f09a3cd
William Pei <draekien@users.noreply.github.com>
2026-02-11 22:28:18 +1100

    feat: add hooks

---
+++ lib/hooks.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+export function useIsMobile(mobileBreakpoint = 768) {
+  const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${mobileBreakpoint - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < mobileBreakpoint);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < mobileBreakpoint);
+    return () => mql.removeEventListener("change", onChange);
+  }, [mobileBreakpoint]);
+
+  return !!isMobile;
+}
+
+export function useCopyToClipboard({
+  timeout = 2000,
+  onCopy,
+}: {
+  timeout?: number;
+  onCopy?: () => void;
+} = {}) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = (value: string) => {
+    if (typeof window === "undefined" || !navigator.clipboard.writeText) {
+      return;
+    }
+
+    if (!value) return;
+
+    navigator.clipboard.writeText(value).then(() => {
+      setIsCopied(true);
+
+      if (onCopy) {
+        onCopy();
+      }
+
+      if (timeout !== 0) {
+        setTimeout(() => {
+          setIsCopied(false);
+        }, timeout);
+      }
+    }, console.error);
+  };
+
+  return { isCopied, copyToClipboard };
+}
