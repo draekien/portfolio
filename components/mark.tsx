8f1fff1f8a441b19e871cf156c05292f5b6c27e1
William Pei <draekien@users.noreply.github.com>
2026-02-11 12:16:12 +1100

    feat: final version of mark

--- components/mark.tsx
+++ components/mark.tsx
@@ -21,19 +21,31 @@ export default function Mark(props: ComponentProps<"svg">) {
     >
       <title>William Pei's Logo</title>
       <path
-        d="M2055.782,114.819h6.012l23.011,54h-6.112Z"
+        d="M2055.782,114.819h6.012l23.011,58h-6.112Z"
         transform="translate(-2032.93 -114.819)"
         fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="2.3"
+        strokeLinejoin="miter"
+        strokeLinecap="butt"
       />
       <path
         d="M0,27.771H0V21.239l13.175-7.353L0,6.532V0L24.877,13.886,0,27.771Z"
         transform="translate(54.961 0)"
         fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="2.3"
+        strokeLinejoin="miter"
+        strokeLinecap="butt"
       />
       <path
-        d="M2055.782,114.819h6.012l23.011,54h-6.112Z"
+        d="M2055.782,114.819h6.012l23.011,58h-6.112Z"
         transform="translate(-2055.782 -114.819)"
         fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="2.3"
+        strokeLinejoin="miter"
+        strokeLinecap="butt"
       />
     </svg>
   );
