2de9d6c6db79545977cb2ea6312c4ded6a202062
William Pei <draekien@users.noreply.github.com>
2026-03-16 12:34:19 +1100

    chore: remove unused components

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,20 +1,4 @@
-import { useEffect, useState } from "react";
-
-export function useIsMobile(mobileBreakpoint = 768) {
-  const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
-
-  useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${mobileBreakpoint - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < mobileBreakpoint);
-    };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < mobileBreakpoint);
-    return () => mql.removeEventListener("change", onChange);
-  }, [mobileBreakpoint]);
-
-  return !!isMobile;
-}
+import { useState } from "react";
 
 export function useCopyToClipboard({
   timeout = 2000,
